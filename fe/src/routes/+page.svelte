efe8e06e698951c91ae8f46cadfe2f1f2dad50f3
Roshan shetty <roshan.aloor@gmail.com>
2023-10-26 00:22:09 +0530

    fix: update api response to table

--- fe/src/routes/+page.svelte
+++ fe/src/routes/+page.svelte
@@ -1,11 +1,14 @@
 <script>
-   import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, 
-    TableHeadCell, Checkbox, TableSearch, Button, Toast } from 'flowbite-svelte';
-   import { CheckCircleSolid, ExclamationCircleSolid, FireOutline, CloseCircleSolid } from 'flowbite-svelte-icons';
+   import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, Radio,
+    TableHeadCell, Checkbox, TableSearch, Button, Toast, GradientButton,
+    Dropdown, DropdownItem, DropdownDivider, DropdownHeader, Search } from 'flowbite-svelte';
+   import { CheckCircleSolid, ExclamationCircleSolid, FireOutline, CloseCircleSolid, ChevronDownSolid, UserRemoveSolid } from 'flowbite-svelte-icons';
    import { onMount } from "svelte";
    import axios from "axios";
 
    let displayToast = false;
+   let regionName = "";
+   let secretName = "";
    let deletedItem = "";
    let tableData = [];
 
@@ -18,7 +21,7 @@
   async function getSecrets() {
     try {
       const response = await axios.get('http://localhost:8080/api/secrets');
-      tableData = response.data;
+      tableData = response.data.data;
     } catch (error) {
       console.error("error fetching secretes", error)
     }
@@ -37,6 +40,7 @@
   });
 
  </script>
+
 
 <div>
 
@@ -58,27 +62,58 @@
     </Toast>
   {/if}
 
-   <!-- TODO: Make it searchable -->
-   <Table hoverable={true} shadow>
-      <TableHead>
-        <TableHeadCell>ID</TableHeadCell>
-        <TableHeadCell>Product name</TableHeadCell>
-        <TableHeadCell>description</TableHeadCell>
-        <TableHeadCell>Action</TableHeadCell>
-      </TableHead>
-      <TableBody>
-        {#each tableData as item}
-          <TableBodyRow>
-            <TableBodyCell>{item.id}</TableBodyCell>
-            <TableBodyCell>{item.name}</TableBodyCell>
-            <TableBodyCell>{item.description}</TableBodyCell>
-            <TableBodyCell>
-              <Button on:click={() => deleteSecretItem(item.id)} color="red" pill>Delete</Button>
-            </TableBodyCell>
-          </TableBodyRow>
-        {/each}
-      </TableBody>
-    </Table>
+  <Button>Select region<ChevronDownSolid class="w-3 h-3 ml-2 text-white dark:text-white" /></Button>
+  <Dropdown class="w-44 p-3 space-y-3 text-sm">
+    <li>
+      <Radio name="regionName" bind:group={regionName} value={"us-east-1"}>us-east-1</Radio>
+    </li>
+    <li>
+      <Radio name="regionName" bind:group={regionName} value={"us-east-2"}>us-east-2</Radio>
+    </li>
+    <li>
+      <Radio name="regionName" bind:group={regionName} value={"us-east-3"}>us-east-3</Radio>
+    </li>
+  </Dropdown>
+
+  <Button>Select secret<ChevronDownSolid class="w-3 h-3 ml-2 text-white dark:text-white" /></Button>
+  <Dropdown class="w-44 p-3 space-y-3 text-sm">
+    <li>
+      <Radio name="secretName" bind:group={secretName} value={"ingester"}>ingester</Radio>
+    </li>
+    <li>
+      <Radio name="secretName" bind:group={secretName} value={"postal"}>postal</Radio>
+    </li>
+    <li>
+      <Radio name="secretName" bind:group={secretName} value={"nile"}>nile</Radio>
+    </li>
+  </Dropdown>
+
+<!-- add few line spaces -->
+  <div class="h-4"></div>
+  
+  <!-- TODO: Make it searchable -->
+  <Table hoverable={true} shadow>
+    <TableHead>
+      <TableHeadCell>ID</TableHeadCell>
+      <TableHeadCell>Product name</TableHeadCell>
+      <TableHeadCell>description</TableHeadCell>
+      <TableHeadCell>Action</TableHeadCell>
+    </TableHead>
+    <TableBody>
+      {#each tableData as item}
+      <TableBodyRow>
+        <TableBodyCell>{item.id}</TableBodyCell>
+        <TableBodyCell>{item.name}</TableBodyCell>
+        <TableBodyCell>{item.description}</TableBodyCell>
+        <TableBodyCell>
+          <Button on:click={() => deleteSecretItem(item.id)} color="red" pill>Delete</Button>
+        </TableBodyCell>
+      </TableBodyRow>
+      {/each}
+    </TableBody>
+  </Table>
+
+  <GradientButton outline pill color="purpleToBlue">Add secret</GradientButton>
 </div>
 
 <style>
--- fe/src/routes/Header.svelte
+++ fe/src/routes/Header.svelte
@@ -5,6 +5,7 @@
 </script>
 
 <header>
+	
 	<div class="corner">
 	</div>
 
