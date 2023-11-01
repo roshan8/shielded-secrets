6eb5eb8534db056dc12cf98d56ef149163c276e5
Roshan shetty <roshan.aloor@gmail.com>
2023-11-01 12:30:06 +0530

    added basic auth

--- pkg/api/routes.go
+++ pkg/api/routes.go
@@ -4,7 +4,7 @@ import (
 	"shielded-secrets/vars"
 
 	"github.com/go-chi/chi/v5"
-
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -12,13 +12,19 @@ func Routes(router chi.Router) {
 	// Endpoint for prometheus to scrape metrics
 	router.Get("/", indexHandler)
 	router.Get("/metrics", promhttp.Handler().ServeHTTP)
-
 	router.Get("/regions", regionHandler)
 	router.With(regionRequired).Route("/{regionID}", initRegionSubRoutes)
 }
 
 func initRegionSubRoutes(router chi.Router) {
 	router.Use(allowOnlyIPs(vars.AllowedIPs))
+
+	if vars.BasicAuthUsername != "" && vars.BasicAuthPassword != "" {
+		router.Use(middleware.BasicAuth("user", map[string]string{
+			vars.BasicAuthUsername: vars.BasicAuthPassword,
+		}))
+	}
+
 	router.Get("/", listSecretsHandler)
 	router.With(secretRequired).Route("/{secretID}", initSecretSubRoutes)
 }
